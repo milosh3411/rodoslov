908eb773f98c68acfabf840d43a0e644fe3d9cc2
Milos Radenkovic <milos.radenkovic@saga.rs>
2020-11-29 19:42:35 +0100

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+//Display width and height
+var w,h;
+// Array of circles' radius values
+var r = [];
+// Array(branches) of arrays (levels) of arrays(nodes)
+var tree = [];
+
 function setup() {
-  // put setup code here
+  // create canvas
+  w = displayWidth;
+  h = displayHeight;
+  createCanvas(w, h);
+
+  r[0] = min(w,h)*0.125;
+
 }
 
 function draw() {
-  // put drawing code here
+  // for each branch
+    // for each level
+      // for each node
+        //draw node
 }
